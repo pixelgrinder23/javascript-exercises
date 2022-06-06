86d7c86ec1da6570c8be5968453e9f4fd9b09013
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-06-06 08:28:14 +0100

    Completed exercise 04 Added html testing file for debugging in browser

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+let toRemoveIndex = 0;
+let slimArray = [];
+let checkArray = [];
 
+const removeFromArray = function(arrayIn) {
+    for (let i = 1; i < arguments.length; i++ ) {
+        toRemoveIndex = arrayIn.indexOf(arguments[i]);
+        if (toRemoveIndex > -1) {
+            slimArray = arrayIn.splice(toRemoveIndex, 1);
+        };
+    };
+    return arrayIn;
 };
+
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+ 

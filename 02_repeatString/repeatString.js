7bc65bb1ea5c7f05f7f75d08c8b9c3749434bee4
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-06-06 06:58:21 +0100

    Completed 01 & 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringRepped = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for(let i = 1; i <= num; i++) {
+            stringRepped = stringRepped + string;
+        };
+    };
+return stringRepped;
 };
 
 // Do not edit below this line

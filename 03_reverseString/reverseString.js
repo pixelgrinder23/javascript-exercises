fd492fece2df6fe81f4720867162ee51d581298b
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-06-06 07:22:31 +0100

    exercise 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringIn) {
+    const revArray = Array.from(stringIn);
+    revArray.reverse();
+    return revArray.join('');
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+ 

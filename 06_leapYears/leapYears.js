8b5f1710e516262988cef0ed7e6769c6b17972fd
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-06-07 06:31:47 +0100

    Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = ((year % 4 === 0) && (year % 100 != 0 || year % 400 === 0)) ? true : false;
+    return result;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+ 
